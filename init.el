f8080ca5dfd53c64897eab1553c8c0bbe8796a81
hanzt <hanzt@hanzt>
2022-10-30 23:43:15 +0800

    add line number setting

--- init.el
+++ init.el
@@ -69,14 +69,15 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  ;;(load-theme 'doom-one t)
+  (load-theme 'doom-one t)
   (load-theme 'doom-acario-dark t)
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  ;;(setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (setq doom-themes-treemacs-theme "doom-one") ; use "doom-colors" for less minimal icon theme
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
@@ -106,5 +107,8 @@
 (ido-mode 1)
 (ido-vertical-mode 1)
 (setq ido-vertical-define-keys 'C-n-and-C-p-only)
+(put 'scroll-left 'disabled nil)
 
-
+;; show line number
+(when (version<= "26.0.50" emacs-version )
+  (global-display-line-numbers-mode))
